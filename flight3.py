db4e60d17c354efb80b51c0ba44e9072aa67ca01
Patrick Yienger <PatrickYienger@Patricks-MacBook-Pro.local>
2018-09-26 13:22:16 -0500

    clean up data

---
+++ flight3.py
@@ -0,0 +1,53 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter('ignore', DeprecationWarning)
+%matplotlib inline
+
+import missingno as mn
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('DallasToChicago.csv') # read in the csv file
+print('Pandas:', pd.__version__)
+print('Numpy:',np.__version__)
+
+#Remove attributes that are not useful for us
+for col in ['Year','Quarter','Month','monthname','DayofMonth','DayOfWeek','TailNum','FlightNum','OriginAirportID',
+           'OriginCityName','OriginState','OriginStateName','DestAirportID','DestCityName','DestState','DestStateName','CRSDepTime',
+           'DepDelayMinutes','TaxiIn','TaxiOut','CRSArrTime','ArrTime','ArrDelay','ArrDelayMinutes','ArrDelayGroup','ATimeBlk','CancellationReason',
+            'Diverted', 'AirTime','CarrierDelay','WeatherDelay','NASDelay','SecurityDelay','LateAircraftDelay','FirstDepTime1','FirstDepTime2',
+            'FirstDepTime','TotalAddGTime','LongestAddGTime','DivAirportLandings','DivReachedDest','DivActualElapsedTime','DivArrDelay','DivDistance',
+           'CRSElapsedTime','Flights','Cancelled','Unnamed: 0', 'Distance', 'DistGroup']:
+    if col in df:
+        del df[col]
+
+print(df.dtypes)
+
+''' Explanation
+
+The data we are focusing on is mainly flight delay time data so we chose to keep the colums of:
+* dayname (the day of the week of the flight) -----------------------> ordinal
+* airline (the airline of the flight)--------------------------------> nominal
+* Origin (origin airport code of the flight)-------------------------> nominal
+* Dest (destination airport code of the flight)----------------------> nominal
+* DepTime (departure time of the flight)-----------------------------> ratio
+* DepDelay (delay of the flight departure in minutes)----------------> interval
+* DepDelayGroup (delay of the flight departure grouped by minutes)---> ordinal
+* DTimeBlk (delay of the flight grouped by hours)--------------------> ordinal
+* ActualElapsedTime (flight time in the air)-------------------------> ratio
+* Distance (distance the flight travelled)---------------------------> interval
+* DistGroup (distance the flight travelled grouped by miles----------> ordinal
+'''
+print(df.info(verbose=True, null_counts=True))
+
+#Figure out what data is missing
+df.isnull().sum()
+
+mn.matrix(df)
+
+#Remove the missing data
+df.dropna(inplace=True)
+df.count()
+df.head()
